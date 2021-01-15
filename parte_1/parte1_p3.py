5686a1f635ef97b81ea3332e04bda41deff53f6f
Randy Martínez <randyma01@gmail.com>
2021-01-15 15:44:21 -0600

    validaciones, impresion de resultados, conexion de modulos, todos faltantes

--- parte_1/parte1_p3.py
+++ parte_1/parte1_p3.py
@@ -36,6 +36,7 @@ import parte1_p2 as metodo
 from tkinter import *
 from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
+
 # from sympy import *
 
 
@@ -47,6 +48,7 @@ def load_image(file):
     """
     Receives the name of an image, loads it from the imgs/.
     Then return the image.
+
     :param file: string
     :return: image
     """
@@ -55,6 +57,7 @@ def load_image(file):
     return image
 
 
+# TODO: corregir posibles errores a la hora de obtener la funcion #
 def get_func_entry():
     func = str(func_entry.get())
     if func == "":
@@ -64,88 +67,149 @@ def get_func_entry():
 
 
 def get_a_entry():
+    """
+    Get entry of the value 'a'.
+
+    If the entry is empty, show message box with error.
+    If the entry is not a integer, show message box with error.
+
+    :return: int
+    """
     a = a_entry.get()
+
     if a == "":
         messagebox.showinfo("¡Error!", "La entrada del valor 'a' no puede estar vacía.")
         return
+
+    try:
+        a = int(a)
+    except ValueError:
+        messagebox.showinfo("¡Error!", "La entrada del valor 'a' debe ser un número entero.")
+        return
+
     return a
 
 
 def get_b_entry():
-    b = str(b_entry.get())
+    """
+    Get entry of the value 'b'.
+
+    If the entry is empty, show message box with error.
+    If the entry is not a integer, show message box with error.
+
+    :return: int
+    """
+    b = b_entry.get()
+
     if b == "":
         messagebox.showinfo("¡Error!", "La entrada del valor 'b' no puede estar vacía.")
         return
-    b = int(b)
+
+    try:
+        b = int(b)
+    except ValueError:
+        messagebox.showinfo("¡Error!", "La entrada del valor 'b' debe ser un número entero.")
+        return
+
     return b
 
 
 def get_points_entry():
-    points = str(b_entry.get())
+    """
+    Get entry of the value 'points'.
+
+    If the entry is empty, show message box with error.
+    If the entry is not a integer, show message box with error.
+
+    :return: int
+    """
+    points = str(points_entry.get())
+
     if points == "":
         messagebox.showinfo("¡Error!", "La entrada de 'Puntos a Utilizar' no puede estar vacía.")
         return
-    points = int(points)
+
+    try:
+        points = int(points)
+    except ValueError:
+        messagebox.showinfo("¡Error!", "La entrada del valor 'b' debe ser un número entero.")
+        return
+
     return points
 
 
-def simp_selection():
-    variable = simp_var.get()
+def method_selection():
+    """
+    Gets the value of the radio button selected
+    for the methods.
+
+    Sets variable 'method' with the value of
+    the option selected.
+
+    :return: method
+    """
+    variable = method_selected.get()
+    method = ""
+
+    if variable == 1:
+        method = "trapecio"
+    elif variable == 2:
+        method = "simpson"
+    elif variable == 3:
+        method = "boole"
+    elif variable == 4:
+        method = "trapecio_compuesto"
+    elif variable == 5:
+        method = "simpson_compuesto"
+    elif variable == 6:
+        method = "gaussianas"
+
+    print(method)
+    return method
+
+
+def calculate():
+    method = method_selection()
     f = get_func_entry()
     a = get_a_entry()
     b = get_b_entry()
-    if variable == 1:
+
+    if method == "trapecio":
         ans = metodo.regla_trapecio(f, a, b)
-        show_results(ans[0], ans[1])
-        return
-    elif variable == 2:
+        approx_cal_label.config(text=ans[0])
+        error_cal_label.config(text=ans[1])
+
+    elif method == "simpson":
         ans = metodo.regla_simpson(f, a, b)
-        show_results(ans[0], ans[1])
-        return
-    elif variable == 3:
+        approx_cal_label.config(text=ans[0])
+        error_cal_label.config(text=ans[1])
+
+    elif method == "boole":
         ans = metodo.regla_boole(f, a, b)
-        show_results(ans[0], ans[1])
-        return
+        approx_cal_label.config(text=ans[0])
+        error_cal_label.config(text=ans[1])
 
+    # TODO: Validacion de metodo que solo recibe una cantidad impar de puntos #
 
-def comp_selection():
-    variable = simp_var.get()
-    f = get_func_entry()
-    a = get_a_entry()
-    b = get_b_entry()
-    points = get_points_entry()
-    if variable == 1:
-        ans = metodo.regla_trapecio_compuesto(f, a, b, points)
-        show_results(ans[0], ans[1])
-        return
-    elif variable == 2:
-        ans = metodo.regla_simpson_compuesto(f, a, b, points)
-        show_results(ans[0], ans[1])
-        return
-    elif variable == 3:
-        ans = metodo.cuadraturas_gausseana(f, a, b, points)
-        show_results(ans[0], ans[1])
-        return
+    elif method == "trapecio_compuesto":
+        n = get_points_entry()
+        ans = metodo.regla_trapecio_compuesto(f, a, b, n)
+        approx_cal_label.config(text=ans[0])
+        error_cal_label.config(text=ans[1])
 
+    elif method == "simpson_compuesto":
+        n = get_points_entry()
+        ans = metodo.regla_simpson_compuesto(f, a, b, n)
+        approx_cal_label.config(text=ans[0])
+        error_cal_label.config(text=ans[1])
 
-def show_results(approx, error):
-    """
-    Display in the main window the values of the approximation and error
-    result inside each corresponding label.
-    :param approx: int
-    :param error: int
-    :return: None
-    """
-    app_label.config(text=approx)
-    err_label.config(text=error)
+    elif method == "gaussianas":
+        n = get_points_entry()
+        ans = metodo.cuadraturas_gausseana(f, a, b, n)
+        approx_cal_label.config(text=ans[0])
+        error_cal_label.config(text=ans[1])
 
-
-# ############## #
-def show():
-    a = get_a_entry()
-    app_label.config(text=a)
-
-# ############## #
+    return
 
 
 # ------------------------------------------------------------------- #
@@ -221,52 +285,51 @@ line_1.place(x=10, y=280)
 tab_control = ttk.Notebook(main_canva, width=500, height=250)
 tab_control.place(x=130, y=300)
 
+# variable for selecting the radio button #
+method_selected = IntVar()
+
 # ------------------------------------------------------------------- #
 #                           simple methods                            #
 # ------------------------------------------------------------------- #
-# variable for control #
-simp_var = IntVar()
-
 # tab for simple methods #
 sim_tab = Frame(tab_control, bg="#FFFFFF")
 tab_control.add(sim_tab, text='Métodos Simples')
 
 # simple trap radio button#
-sim_trap = Radiobutton(sim_tab, command=simp_selection, variable=simp_var, text="Trapecio", bg="#FFFFFF", value=1)
+sim_trap = Radiobutton(sim_tab, command=method_selection, variable=method_selected, text="Trapecio",
+                       bg="#FFFFFF", value=1)
 sim_trap.place(x=200, y=50)
 
 # simple simp radio button #
-sim_simp = Radiobutton(sim_tab, command=simp_selection, variable=simp_var, text="Simpson", bg="#FFFFFF", value=2)
+sim_simp = Radiobutton(sim_tab, command=method_selection, variable=method_selected, text="Simpson",
+                       bg="#FFFFFF", value=2)
 sim_simp.place(x=200, y=100)
 
 # boole radio button #
-sim_boole = Radiobutton(sim_tab, command=simp_selection, variable=simp_var, text="Regla de Boole", bg="#FFFFFF",
-                        value=3)
+sim_boole = Radiobutton(sim_tab, command=method_selection, variable=method_selected, text="Regla de Boole",
+                        bg="#FFFFFF", value=3)
 sim_boole.place(x=200, y=150)
 
 # ------------------------------------------------------------------- #
 #                             comp methods                            #
 # ------------------------------------------------------------------- #
-# variable for control #
-comp_var = IntVar()
-
 # tabs for comp methods #
 comp_tab = Frame(tab_control, bg="#FFFFFF")
 tab_control.add(comp_tab, text='Métodos Compuestos')
 
 # comp trap radio button#
-comp_trap = Radiobutton(comp_tab, command=comp_selection, variable=comp_var, text="Trapecio Compuesto", bg="#FFFFFF",
-                        value=4)
+comp_trap = Radiobutton(comp_tab, command=method_selection, variable=method_selected, text="Trapecio Compuesto",
+                        bg="#FFFFFF", value=4)
 comp_trap.place(x=70, y=50)
 
 # comp simp radio button #
-comp_simp = Radiobutton(comp_tab, command=comp_selection, variable=comp_var, text="Simpson Compuesto", bg="#FFFFFF",
-                        value=5)
+comp_simp = Radiobutton(comp_tab, command=method_selection, variable=method_selected, text="Simpson Compuesto",
+                        bg="#FFFFFF", value=5)
 comp_simp.place(x=70, y=100)
 
 # cuad gaus radio button #
-cuad_gau = Radiobutton(comp_tab, command=comp_selection, variable=comp_var, text="Cuadraturas Gaussianas", bg="#FFFFFF",
-                       value=6)
+cuad_gau = Radiobutton(comp_tab, command=method_selection, variable=method_selected, text="Cuadraturas Gaussianas",
+                       bg="#FFFFFF", value=6)
 cuad_gau.place(x=70, y=150)
 
 # points entry #
@@ -286,29 +349,29 @@ line_2.place(x=10, y=630)
 #                              calculator                             #
 # ------------------------------------------------------------------- #
 # calculate button #
-calculate_button = Button(main_canva, command=show, borderwidth=0, text="Calcular", bg="#FFFFFF", fg="#0000FF",
+calculate_button = Button(main_canva, command=calculate, borderwidth=0, text="Calcular", bg="#FFFFFF", fg="#0000FF",
                           font=("Courier", 20, "italic"))
 calculate_button.place(x=340, y=650)
 
 # approximation label #
-approx_label = Label(main_canva, text="Aproximación =", bg="#FFFFFF", fg="#000000",
-                     font=("Courier", 18))
-approx_label.place(x=150, y=700)
+approx_title_label = Label(main_canva, text="Aproximación =", bg="#FFFFFF", fg="#000000",
+                           font=("Courier", 18))
+approx_title_label.place(x=150, y=700)
 
 # calculated answer label #
-app_label = Label(main_canva, bg="#FFFFFF", fg="#000000",
-                  font=("Courier", 18))
-app_label.place(x=345, y=700)
+approx_cal_label = Label(main_canva, bg="#FFFFFF", fg="#000000",
+                         font=("Courier", 18))
+approx_cal_label.place(x=345, y=700)
 
 # error label #
-error_label = Label(main_canva, text="Error =", bg="#FFFFFF", fg="#000000",
-                    font=("Courier", 18))
-error_label.place(x=150, y=750)
+error_title_label = Label(main_canva, text="Error =", bg="#FFFFFF", fg="#000000",
+                          font=("Courier", 18))
+error_title_label.place(x=150, y=750)
 
 # calculated error label #
-err_label = Label(main_canva, bg="#FFFFFF", fg="#000000",
-                  font=("Courier", 18))
-err_label.place(x=345, y=750)
+error_cal_label = Label(main_canva, bg="#FFFFFF", fg="#000000",
+                        font=("Courier", 18))
+error_cal_label.place(x=345, y=750)
 
 # ------------------------------------------------------------------- #
 #                           division line                             #
